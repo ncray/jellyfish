90d2a9cf3a48950a54df3d3bb32882f30d811d34
Nelson Ray <ncray86@gmail.com>
2013-07-20 18:54:52 -0700

    first parallel version working

--- plot_jelly.jl
+++ plot_jelly.jl
@@ -10,7 +10,7 @@ Pkg.add("Calculus")
 Pkg.add("Distributions")
 Pkg.add("Stats")
 
-include("jellyfish.jl")
+include("src/jellyfish.jl")
 
 M = get_low_rank(100, 100, 3)
 U, S, V = svd(M)
@@ -20,20 +20,33 @@ M2 = jellyfish_1(M, num_iter = 100, L = U * sqrt(diagm(S)), R = (sqrt(diagm(S)) 
 @assert rel_err(M, M2) < .01
 
 using Benchmark
-nrow = 100
-ncol = 100
+nrow = 200
+ncol = 200
 num_iter = 10
 M = get_low_rank(nrow, ncol, 3)
 
 rel_err(jellyfish_1(M, num_iter = num_iter * nrow * ncol)[1], M)
 rel_err(jellyfish_2(M, num_iter = num_iter)[1], M)
+rel_err(jellyfish_3(M, num_iter = num_iter)[1], M)
+rel_err(jellyfish_3(M, num_iter = num_iter, p = 1)[1], M)
+rel_err(jellyfish_4(M, num_iter = num_iter, p = 3)[1], M)
+
+include("src/jellyfish.jl")
+require("src/jellyfish.jl")
+[rel_err(jellyfish_3(M, num_iter = num_iter, p = i)[1], M) for i in 1:100]
+
 
 j1() = jellyfish_1(M, num_iter = num_iter * nrow * ncol)
 j2() = jellyfish_2(M, num_iter = num_iter)
+j3() = jellyfish_3(M, num_iter = num_iter, p = 1)
+j4() = jellyfish_4(M, num_iter = num_iter, p = 3)
+
 benchmark(j1, "j1", 3)
 benchmark(j2, "j2", 3)
-
-compare([j1, j2], 4)
+benchmark(j3, "j3", 3)
+benchmark(j4, "j4", 3)
+    
+compare([j1, j2, j3, j4], 4)
 
 ## using Gadfly
 ## using DataFrames
--- src/jellyfish.jl
+++ src/jellyfish.jl
@@ -41,7 +41,7 @@ function jellyfish_2(M,                        # target matrix
                      r = 3;                    # guess of rank
                      mu = 1,                   # regularization parameter
                      alpha_k = .01,            # learning rate
-                     num_iter = 10000,         # number of iterations
+                     num_iter = 10,            # number of iterations
                      L = randn(size(M, 1), r), # initial guess for L
                      R = randn(size(M, 2), r)) # initial guess for R
     n_r = size(M, 1)
@@ -85,62 +85,134 @@ function get_chunks(n_r, n_c, p)
     (C_a, C_b)
 end
 
-function gradient_updates(M, L, R, n_r, n_c, r, mu, alpha_k)
-    n_r = size(M, 1)
-    n_c = size(M, 2)
-
-    for i in 1:n_r
-        for j in 1:n_c
+function gradient_updates(M, L, R, r, mu, alpha_k, n_r, n_c)
+    n_r_L = size(L, 1)
+    n_c_R = size(R, 1)
+    
+    for i in 1:n_r_L
+        for j in 1:n_c_R
             resid = alpha_k * 2 * (L[i, :] * R[j, :]' - M[i, j])
             L[i, :], R[j, :] = (1 - mu * alpha_k / n_r) * L[i, :] - resid * R[j, :],
             (1 - mu * alpha_k / n_c) * R[j, :] - resid * L[i, :]
         end
     end
+    
     (L, R)
 end
 
+## M = reshape(linspace(1, 100, 100), 10, 10)'
+## function foo(x)
+##     x[1] = -1
+## end
+## foo(M[1, :])
+## M
 #gradient_updates(X, 3, 1, .01)
 #chunks = get_chunks(10, 10, 3)
 
-
-## basic version
-## permute rows/columns
 function jellyfish_3(M,                        # target matrix
                      r = 3;                    # guess of rank
                      p = 3,                    # number of processors
                      mu = 1,                   # regularization parameter
                      alpha_k = .01,            # learning rate
-                     num_iter = 10000,         # number of iterations
+                     num_iter = 10,            # number of iterations
                      L = randn(size(M, 1), r), # initial guess for L
-                     R = randn(size(M, 2), r), # initial guess for R
-                     all_errs = true)
+                     R = randn(size(M, 2), r)) # initial guess for R
     n_r = size(M, 1)
     n_c = size(M, 2)
     chunks = get_chunks(n_r, n_c, p)
-
+    
     for k = 1:num_iter
-        for l in 0:(p-1)
+        for l in 0:(p - 1)
             ##a = [1:p]
             ##b = [((x + l) % p) + 1 for x in a]
             ab = [(x, ((x + l) % p) + 1) for x in [1:p]]
             for (i, j) in ab
                 inds = chunks[1][i], chunks[2][j]
-                L[inds[1], 1:r], R[inds[2], 1:r] = gradient_updates(M[inds[1], inds[2]], L[inds[1], 1:r], R[inds[2], 1:r], n_r, n_c, r, mu, alpha_k)
+                L[inds[1], 1:r], R[inds[2], 1:r] = gradient_updates(M[inds[1], inds[2]], L[inds[1], 1:r], R[inds[2], 1:r], r, mu, alpha_k, n_r, n_c)
             end
         end
     end
-    L * R'
+    (L * R', L, R)
 end
 
+## X = {rand(1000,1000), rand(1000,1000), rand(1000,1000), rand(1000,1000)}
+## @elapsed pmap(svd, X)
+## @elapsed svd(rand(1000, 1000))
 
+## addprocs(3)
+## tic()
+## @parallel for i = 1:4
+##     svd(rand(2000, 2000))
+## end
+## s = toc()
+## println(s)
 
+## @elapsed svd(rand(2000, 2000))
 
+## pmap((x)->x^2, [1:4])
 
+function jellyfish_4(M,                        # target matrix
+                     r = 3;                    # guess of rank
+                     p = 3,                    # number of processors
+                     mu = 1,                   # regularization parameter
+                     alpha_k = .01,            # learning rate
+                     num_iter = 10,            # number of iterations
+                     L = randn(size(M, 1), r), # initial guess for L
+                     R = randn(size(M, 2), r)) # initial guess for R
+    n_r = size(M, 1)
+    n_c = size(M, 2)
+    chunks = get_chunks(n_r, n_c, p)
+    
+    for k = 1:num_iter
+        for l in 0:(p - 1)
+            ab = [(x, ((x + l) % p) + 1) for x in [1:p]]
+            refs = RemoteRef[]
+            
+            for (i, j) in ab
+                inds = chunks[1][i], chunks[2][j]
+                push!(refs, @spawn gradient_updates(M[inds[1], inds[2]], L[inds[1], 1:r], R[inds[2], 1:r], r, mu, alpha_k, n_r, n_c))
+            end
+                      
+            for (i, j) in ab
+                inds = chunks[1][i], chunks[2][j]
+                L[inds[1], 1:r], R[inds[2], 1:r] = fetch(shift!(refs))
+            end                
+        end
+    end
+    (L * R', L, R)
+end
 
+## function grad_wrapper(inds)
+##     gradient_updates(M[inds[1], inds[2]], L[inds[1], 1:r], R[inds[2], 1:r], r, mu, alpha_k, n_r, n_c)
+## end
 
+## function jellyfish_4(M,                        # target matrix
+##                      r = 3;                    # guess of rank
+##                      p = 3,                    # number of processors
+##                      mu = 1,                   # regularization parameter
+##                      alpha_k = .01,            # learning rate
+##                      num_iter = 10,            # number of iterations
+##                      L = randn(size(M, 1), r), # initial guess for L
+##                      R = randn(size(M, 2), r)) # initial guess for R
+##     n_r = size(M, 1)
+##     n_c = size(M, 2)
+##     chunks = get_chunks(n_r, n_c, p)
 
-
-
+##     for k = 1:num_iter
+##         for l in 0:(p - 1)
+##             ab = [(x, ((x + l) % p) + 1) for x in [1:p]]
+##             all_inds = [(chunks[1][i], chunks[2][j]) for (i, j) in ab]
+##                 res = pmap(grad_wrapper, all_inds)
+##                 counter = 1
+##             for (i, j) in ab
+##                 inds = chunks[1][i], chunks[2][j]
+##                 L[inds[1], 1:r], R[inds[2], 1:r] = res[counter]
+##                 counter += 1
+##             end
+##         end
+##     end
+##     (L * R', L, R)
+## end
 
 
 
